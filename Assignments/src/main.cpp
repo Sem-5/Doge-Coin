ac00fd1352123b6835ae2c107d7e90d6f4511448
maverick6130 <daror6130@gmail.com>
2021-09-07 19:32:43 +0530

    added command line parameters to main

--- Assignments/src/main.cpp
+++ Assignments/src/main.cpp
@@ -1,7 +1,26 @@
 #include "NetSim.hpp"
 
-int main()
+#include <iostream>
+#include <sstream>
+
+int main(int argc, char*argv[])
 {
-    NetSim ns(20, 0.5, 5e3, 60e3);
-    ns.simulate(600e3);
+    if (argc != 6)
+    {
+        std::cout << "Format : ./sim nNodes fracSlow TxnTime BlockTime SimulationEnd" << std::endl;
+        return 1;
+    }
+    int nNodes;
+    double fracSlow;
+    double TxnTime;
+    double BlockTime;
+    double SimulationEnd;
+    std::stringstream(argv[1]) >> nNodes;
+    std::stringstream(argv[2]) >> fracSlow;
+    std::stringstream(argv[3]) >> TxnTime;
+    std::stringstream(argv[4]) >> BlockTime;
+    std::stringstream(argv[5]) >> SimulationEnd;
+    
+    NetSim ns(nNodes, fracSlow, TxnTime, BlockTime);
+    ns.simulate(SimulationEnd);
 }
